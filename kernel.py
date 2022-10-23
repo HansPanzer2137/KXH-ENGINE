4662c5b582168c8e092f330f2b4951cd6efb0b91
Majster <74077594+HansPanzer2137@users.noreply.github.com>
2022-10-23 15:35:34 +0200

    DOOM update

--- kernel.py
+++ kernel.py
@@ -3,7 +3,11 @@ import sys
 
 
 from bib import main_pip
+from bib.gui.experimental.doom import main
+
 
 #print(sys.modules) debug option (gives info about imported files/modules)
 
-main_pip.engine()
+
+main.Doom_Run()
+#main_pip.engine() engine
